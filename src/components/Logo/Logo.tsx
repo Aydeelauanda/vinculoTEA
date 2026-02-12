13c62b426f16e43b29cb6c6bd2ce48d8ffd587a6
Aydeelauanda <156032159+Aydeelauanda@users.noreply.github.com>
2026-02-12 01:20:10 +0000

    feat: implementação das novas funcionalidades (Tabs, PEI W izard e UI renovada)

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -8,7 +8,7 @@ export const Logo: React.FC = () => {
     <div className={styles.container}>
       <img 
         src={logoImg}  
-        alt="EduTutor PEI Logo" 
+        alt="VinculoTEA Logo" 
         className={styles.image}
       />
     </div>
